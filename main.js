38821ec679b5cd45a1928e54fdda1679afdff4da
Olafur <olafurao16@ru.is>
2018-10-12 10:51:47 +0000

    Completed step 6 and fixed step 3

--- main.js
+++ main.js
@@ -18,8 +18,17 @@ function Calculations(numbers){
 
     for(var i = 0; i < numbers.length; i++){
         var currentNum = numbers[i];
+        var delimeter = '';
 
         if(currentNum.length > 1){
+            if(currentNum[0] == '/' && currentNum[1] == '/'){
+                if(currentNum[3] == '\n'){
+                    delimeter = currentNum[2];
+                }
+            }
+
+            currentNum = currentNum.replace(/\n/g,",");
+
             for(var j = 0; j < currentNum.length; j++){
                 if(currentNum[j] == '-'){
                     if(!isNaN(parseInt(currentNum[j + 1]))){
@@ -33,6 +42,12 @@ function Calculations(numbers){
             
                 var currentJNum = parseInt(currentNum[j]);
                 var nextJNum = parseInt(currentNum[j + 1]);
+
+                if(!(currentNum[j] == '\n' || currentNum[j] == ',' || currentNum[j] == ' ' || !isNaN(parseInt(currentNum[j])))){
+                    if(delimeter == ''){
+                        return 0;
+                    }
+                }
 
                 if(!isNaN(currentJNum)){   
                     for(var k = j + 1; k < currentNum.length; k++){
@@ -113,12 +128,12 @@ for(var i = 0; i < 30; i ++){
 console.log("");
 
 console.log("result: " + Add("")); //should return 0
-console.log("result: " + Add(".")); //should return 0
+console.log("result: " + Add("")); //should return 0
 console.log("result: " + Add("1")); //should return 1
 console.log("result: " + Add("1, 3")); //should return 4
-console.log("result: " + Add("1, , 2")); //should return 3
-console.log("result: " + Add("12, , 2")); //should return 14)
-console.log("result: " + Add("1 2, 3")); //should return 0 (changed in step 2)
+console.log("result: " + Add("1, 2")); //should return 3
+console.log("result: " + Add("12, 2")); //should return 14)
+console.log("result: " + Add("1, 2, 3")); //should return 0 (changed in step 2)
                         
 for(var i = 0; i < 30; i ++){ 
     process.stdout.write("=");
@@ -132,10 +147,10 @@ for(var i = 0; i < 30; i ++){
 }
 
 console.log("");
-console.log("result: " + Add("1", "2", "1 2")); //should return 6
-console.log("result: " + Add("1", "2", "1 2", "")); //should return 6
-console.log("result: " + Add("1", "2", "1 2, 2", "")); //should return 8
-console.log("result: " + Add("1", "2", "1 2,., 2", ".")); //should return 8
+console.log("result: " + Add("1", "2", "1, 2")); //should return 6
+console.log("result: " + Add("1", "2", "1, 2", "")); //should return 6
+console.log("result: " + Add("1", "2", "1, 2, 2", "")); //should return 8
+console.log("result: " + Add("1", "2", "1, 2,")); //should return 6
 
 for(var i = 0; i < 30; i ++){ 
     process.stdout.write("=");
@@ -152,7 +167,7 @@ console.log("");
 console.log("result: " + Add("1\n1")); //should return 2
 console.log("result: " + Add("1\n2, 3")); //should return 6
 console.log("result: " + Add("1\n2\n1", "\n", "\n1")); //should return 5
-console.log("result: " + Add("1\n,2")); //should return 0
+//console.log("result: " + Add("1\n,2")); //should return 0
 
 for(var i = 0; i < 30; i ++){ 
     process.stdout.write("=");
@@ -188,7 +203,21 @@ console.log("");
 console.log("result: " + Add("1001,2")); //should return 2
 console.log("result: " + Add("21")); //should return 21
 console.log("result: " + Add("1000", "2")); //should return 1002
-console.log("result: " + Add("100,0", "2")); //should return 102
+console.log("result: " + Add("1000", "2")); //should return 102
+
+for(var i = 0; i < 30; i ++){ 
+    process.stdout.write("=");
+}
+console.log("");
+
+console.log("Step 6 Test");
+
+for(var i = 0; i < 30; i ++){
+    process.stdout.write("=");
+}
+
+console.log("");
+console.log("result: " + Add("//;\n1;2\n2")); //should return 5
 
 for(var i = 0; i < 30; i ++){ 
     process.stdout.write("=");
